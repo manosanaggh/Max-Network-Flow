9e08f956ef1726d9ca230b076200809ff7cb9358
Manos Anagnostakis <manos.anagnostakis97@gmail.com>
2025-03-13 17:05:02 +0200

    feat: add simple test

--- src/Application.java
+++ src/Application.java
@@ -4,7 +4,7 @@ class Application{
 	public static void main(String[] args){
 		DirectedGraph dg = new DirectedGraph();
 		InputParser parser = new InputParser();
-		parser.parseInput("input1.txt", dg);
+		parser.parseInput("input/input1.txt", dg);
 		dg.printVertices();		
 	}
 }
