fc939fe29f377fe2a3ca2b3538def2d2caff18ec
Manos Anagnostakis <manos.anagnostakis97@gmail.com>
2025-03-13 17:03:20 +0200

    feat: add Application class

---
+++ src/Application.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+class Application{
+	public static void main(String[] args){
+		DirectedGraph dg = new DirectedGraph();
+		InputParser parser = new InputParser();
+		parser.parseInput("input1.txt", dg);
+		dg.printVertices();		
+	}
+}
