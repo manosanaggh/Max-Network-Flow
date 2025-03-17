6117a44efb3b033a641f5e70c73bf887c5965bca
Manos Anagnostakis <manos.anagnostakis97@gmail.com>
2025-03-17 18:07:44 +0200

    fix: Call parseInput as static.

--- src/Application.java
+++ src/Application.java
@@ -3,8 +3,7 @@ import java.util.ArrayList;
 class Application{
 	public static void main(String[] args){
 		DirectedGraph dg = new DirectedGraph();
-		InputParser parser = new InputParser();
-		parser.parseInput("input/input1.txt", dg);
+		InputParser.parseInput("input/input1.txt", dg);
 		dg.printVertices();		
 	}
 }
