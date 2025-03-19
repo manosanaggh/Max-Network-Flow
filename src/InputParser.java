2f8057592c416347555c80e464aab172223a3527
Manos Anagnostakis <manos.anagnostakis97@gmail.com>
2025-03-19 12:21:12 +0200

    fix: Final version of InputParser class without comments.

--- src/InputParser.java
+++ src/InputParser.java
@@ -6,10 +6,18 @@ class InputParser{
 		try (final BufferedReader br = new BufferedReader (new FileReader (filename))){
 			String line;
 			final int numVertices = Integer.parseInt (br.readLine().trim());
+			for (int i = 0; i < numVertices; i++){
+				dg.insertVertex (i);	
+			}
 			while ((line = br.readLine()) != null){
 				final String[] parts = line.split ("\\s+");
 				final int[] numbers = Arrays.stream(parts).mapToInt(Integer::parseInt).toArray();
-				dg.insertVertex (numbers[0], numbers[1], numbers[2]);
+				Edge e0 = new Edge (numbers[1], numbers[2]);
+				Edge e1 = new Edge (numbers[0], 0);
+				e0.setReverse(e1);
+				e1.setReverse(e0);
+				dg.getVertices().get(numbers[0]).insertEdge(e0);
+				dg.getVertices().get(numbers[1]).insertEdge(e1);
 			}
 		} catch (IOException | NumberFormatException e){
 			e.printStackTrace();
