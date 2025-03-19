15d7eec5d5616e468b8887d396a57dd025ed4ecd
Manos Anagnostakis <manos.anagnostakis97@gmail.com>
2025-03-19 12:20:51 +0200

    fix: Final version of Edge class without comments.

--- src/Edge.java
+++ src/Edge.java
@@ -1,10 +1,14 @@
 class Edge{
 	private int destinationVertexName;
 	private int capacity;
+	private int flow;
+	private Edge reverse;
 
 	Edge(final int name, final int capacity){
 		destinationVertexName = name;
 		this.capacity = capacity;
+		flow = 0;
+		this.reverse = null;
 	}
 
         final int getName(){
@@ -14,6 +18,22 @@ class Edge{
         final int getCapacity(){
                 return capacity;
         }
+
+	final void setReverse(Edge reverse){
+		this.reverse = reverse;
+	}
+
+	final Edge getReverse(){
+		return reverse;
+	}
+
+	final void setFlow(int flow){
+		this.flow = flow;
+	}
+
+	final int getFlow(){
+		return flow;
+	}
 
         @Override public String toString(){
                 return "("+destinationVertexName+","+capacity+")";
